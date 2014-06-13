cd94e9f3c376cd3b74f300da53bc57962c6401ed
almazbeck <almaz.dzhumabaev@iaau.edu.kg>
2014-06-13 19:23:34 +0600

    ofiste

--- javascript/schedule.js
+++ javascript/schedule.js
@@ -4,7 +4,15 @@
 
 function close_schedule(id){
     document.getElementById(id).style.display="none";
-    }
+}
 function f_schedule(id){
     document.getElementById(id).style.display="block";
-    }
+
+
+
+}
+var myvar;
+myvar="Hello world";
+alert(myvar);
+
+
